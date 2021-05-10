f6507fb0c40b00c6ec1986c27a0121d9567995c2
Hannah White <WHITEHA@tcd.ie>
2021-05-10 17:07:07 +0100

    Add additional name changes

--- CreateSpeciesTraitMatrix.R
+++ CreateSpeciesTraitMatrix.R
@@ -30,6 +30,9 @@ birds$species <- gsub('Emberiza_calandra', 'Miliaria_calandra', birds$species)
 birds$species <- gsub('Chloris_chloris', 'Carduelis_chloris', birds$species)
 birds$species <- gsub('Poecile_palustris', 'Parus_palustris', birds$species)
 birds$species <- gsub('Iduna_pallida', 'Hippolais_pallida', birds$species)
+birds$species <- gsub('Saxicola_rubetrus', 'Saxicola_rubetra', birds$species)
+birds$species <- gsub('Saxocola_torquatus', 'Saxicola_torquatus', birds$species)
+
 
 #### Traits
 
