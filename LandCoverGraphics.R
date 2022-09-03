4f55ab8046ca87fa7459572260a3f362942e812c
Sam Ross <72502982+SamRPJRoss@users.noreply.github.com>
2022-09-03 11:50:34 +0900

    updated figures

--- LandCoverGraphics.R
+++ LandCoverGraphics.R
@@ -12,9 +12,11 @@ library(multcomp)
 library(ggplot2)
 library(RColorBrewer)
 library(cowplot)
+library(scales)
+library(tidyverse)
 
 ## Read in data
-setwd('')
+setwd("~/Documents/GitHub/Trait-diversity-stability")
 
 
 load('bird.long.RData')
@@ -51,15 +53,29 @@ ci.response <-  confint(glht(gls.response, linfct = mcp(majority = "Tukey"))) # 
 mc.response <- glht(gls.response, linfct = mcp(majority = "Tukey"))
 
 ## Figure
-p.response <- ggplot(bird.long, aes(x = majority, y = response.fdis, colour = majority)) + geom_boxplot()
-p.response <- p.response + xlab(NULL) + ylab('Response Trait FDis')
-p.response <- p.response + scale_color_brewer(palette = "Dark2", direction = -1)
-p.response <- p.response + theme_classic() + theme(legend.position = 'none', 
-                                                   axis.text = element_text(size = 14),
-                                                   axis.title.y = element_text(size = 14))
-p.response <- p.response + scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural'))
-p.response <- p.response + annotate('text',x = 1, y = 0.138, label = 'a')
-p.response <- p.response + annotate('text', x = c(2, 3), y = c(0.138, 0.138), label = 'b')
+
+p.response <-
+  bird.long %>% 
+  ggplot(aes(x = majority, y = response.fdis, colour = majority)) +
+  theme_classic(base_size = 14) + 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 14, colour = 'black'),
+        axis.title.y = element_text(size = 14)) +
+  labs(x = NULL, y = 'Response diversity') + 
+  geom_point(aes(shape = majority),col = 'black',
+             position = position_jitter(width = 0.35),
+             size = 2,
+             show.legend = FALSE) + 
+  geom_boxplot(aes(fill = majority), col = 'black',
+               alpha = 0.6) + 
+  scale_color_manual(values = c('#fd6f77','#bd95b2','#00b0f0'),aesthetics = c('colour','fill')) +
+  #scale_color_brewer(palette = "Dark2", direction = -1,aesthetics = c('colour','fill')) + 
+  scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural')) + 
+  annotate('text',x = 1, y = 0.138, label = 'a') + 
+  annotate('text', x = c(2, 3), y = c(0.138, 0.138), label = 'b') +
+  scale_y_continuous(breaks = breaks_pretty(n = 5)) + 
+  scale_shape_manual(values = c(22,2,1)) +
+  theme(legend.position = "none")
 
 
 ## Effect trait diversity
@@ -67,81 +83,147 @@ gls.effect <- gls(effect.fdis ~ majority, data = bird.long,
                   corr = corGaus(form = ~xcoord + ycoord, nugget = TRUE))
 ci.effect <- confint(glht(gls.effect, linfct = mcp(majority = "Tukey"))) # forest not different from agri but both different from urban
 
-p.effect <- ggplot(bird.long, aes(x = majority, y = effect.fdis, colour = majority)) + geom_boxplot()
-p.effect <- p.effect + xlab(NULL) + ylab('Effect Trait FDis')
-p.effect <- p.effect + scale_color_brewer(palette = "Dark2", direction = -1)
-p.effect <- p.effect + theme_classic() + theme(legend.position = 'none', 
-                                               axis.text = element_text(size = 14),
-                                               axis.title.y = element_text(size = 14))
-p.effect <- p.effect + scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural'))
-p.effect <- p.effect + annotate('text',x = 1, y = 0.17, label = 'a')
-p.effect <- p.effect + annotate('text', x = c(2, 3), y = c(0.17, 0.17), label = 'b')
-
+p.effect <-
+  bird.long %>% 
+  ggplot(aes(x = majority, y = effect.fdis, colour = majority)) +
+  theme_classic(base_size = 14) + 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 14, colour = 'black'),
+        axis.title.y = element_text(size = 14)) +
+  labs(x = NULL, y = 'Effect diversity') + 
+  geom_point(aes(shape = majority),col = 'black',
+             position = position_jitter(width = 0.35),
+             size = 2,
+             show.legend = FALSE) + 
+  geom_boxplot(aes(fill = majority), col = 'black',
+               alpha = 0.6) + 
+  scale_color_manual(values = c('#fd6f77','#bd95b2','#00b0f0'),aesthetics = c('colour','fill')) +
+  #scale_color_brewer(palette = "Dark2", direction = -1,aesthetics = c('colour','fill')) + 
+  scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural')) + 
+  annotate('text',x = 1, y = 0.17, label = 'a') + 
+  annotate('text', x = c(2, 3), y = c(0.17, 0.17), label = 'b') +
+  scale_y_continuous(breaks = breaks_pretty(n = 5)) + 
+  scale_shape_manual(values = c(22,2,1)) +
+  theme(legend.position = "none")
 
 ## Effect trait variability
 gls.effectvar <- gls(log(cv.fdis) ~ majority, data = bird.long,
                      corr = corGaus(form = ~xcoord + ycoord, nugget = TRUE))
 ci.effectvar <- confint(glht(gls.effectvar, linfct = mcp(majority = "Tukey"))) # forest significantly different from agriculture
 
-p.effectvar <- ggplot(bird.long, aes(x = majority, y = log(cv.fdis), colour = majority)) + geom_boxplot()
-p.effectvar <- p.effectvar + xlab(NULL) + ylab('Effect Trait FDis Variability (log)')
-p.effectvar <- p.effectvar + scale_color_brewer(palette = "Dark2", direction = -1)
-p.effectvar <- p.effectvar + theme_classic() + theme(legend.position = 'none', 
-                                                     axis.text = element_text(size = 14),
-                                                     axis.title.y = element_text(size = 14))
-p.effectvar <- p.effectvar + scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural'))
-p.effectvar <- p.effectvar + annotate('text',x = c(1,2), y = c(-0.65, -0.65), label = 'a')
-p.effectvar <- p.effectvar + annotate('text', x =  3, y = -0.65, label = 'b')
+p.effectvar <-
+  bird.long %>% 
+  ggplot(aes(x = majority, y = log(cv.fdis), colour = majority)) +
+  theme_classic(base_size = 14) + 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 14, colour = 'black'),
+        axis.title.y = element_text(size = 14)) +
+  labs(x = NULL, y = 'Effect diversity variability (ln)') + 
+  geom_point(aes(shape = majority),col = 'black',
+             position = position_jitter(width = 0.35),
+             size = 2,
+             show.legend = FALSE) + 
+  geom_boxplot(aes(fill = majority), col = 'black',
+               alpha = 0.6) + 
+  scale_color_manual(values = c('#fd6f77','#bd95b2','#00b0f0'),aesthetics = c('colour','fill')) +
+  #scale_color_brewer(palette = "Dark2", direction = -1,aesthetics = c('colour','fill')) + 
+  scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural')) + 
+  annotate('text',x = c(1,2), y = c(-0.65, -0.65), label = 'a') + 
+  annotate('text', x =  3, y = -0.65, label = 'b') +
+  scale_y_continuous(breaks = breaks_pretty(n = 5)) + 
+  scale_shape_manual(values = c(22,2,1)) +
+  theme(legend.position = "none")
+
 
 ## Asynchrony
 gls.asyn <- gls(Com.Asynch ~ majority, data = bird.long,
                 corr = corGaus(form = ~xcoord + ycoord, nugget = TRUE))
 ci.asyn <- confint(glht(gls.asyn, linfct = mcp(majority = "Tukey"))) # no differences
 
-p.asyn <- ggplot(bird.long, aes(x = majority, y = Com.Asynch, colour = majority)) + geom_boxplot()
-p.asyn <- p.asyn + xlab(NULL) + ylab('Community Asynchony')
-p.asyn <- p.asyn + scale_color_brewer(palette = "Dark2", direction = -1)
-p.asyn <- p.asyn + theme_classic() + theme(legend.position = 'none', 
-                                           axis.text = element_text(size = 14),
-                                           axis.title.y = element_text(size = 14))
-p.asyn <- p.asyn + scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural'))
+p.asyn <-
+  bird.long %>% 
+  ggplot(aes(x = majority, y = Com.Asynch, colour = majority)) +
+  theme_classic(base_size = 14) + 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 14, colour = 'black'),
+        axis.title.y = element_text(size = 14)) +
+  labs(x = NULL, y = 'Community asynchrony') + 
+  geom_point(aes(shape = majority),col = 'black',
+             position = position_jitter(width = 0.35),
+             size = 2,
+             show.legend = FALSE) + 
+  geom_boxplot(aes(fill = majority), col = 'black',
+               alpha = 0.6) + 
+  scale_color_manual(values = c('#fd6f77','#bd95b2','#00b0f0'),aesthetics = c('colour','fill')) +
+  #scale_color_brewer(palette = "Dark2", direction = -1,aesthetics = c('colour','fill')) + 
+  scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural')) + 
+  scale_y_continuous(breaks = breaks_pretty(n = 5)) + 
+  scale_shape_manual(values = c(22,2,1)) +
+  theme(legend.position = "none")
+
 
 ## Compositional variability
 gls.temp <- gls(Temp_Var ~ majority, data = bird.long,
                 corr = corGaus(form = ~xcoord + ycoord, nugget = TRUE))
 ci.temp <- confint(glht(gls.temp, linfct = mcp(majority = "Tukey"))) # forest and agri same but both different from urban
 
-p.temp <- ggplot(bird.long, aes(x = majority, y = Temp_Var, colour = majority)) + geom_boxplot()
-p.temp <- p.temp + xlab(NULL) + ylab('Compositional Temporal Variability')
-p.temp <- p.temp + scale_color_brewer(palette = "Dark2", direction = -1)
-p.temp <- p.temp + theme_classic() + theme(legend.position = 'none', 
-                                           axis.text = element_text(size = 14),
-                                           axis.title.y = element_text(size = 14))
-p.temp <- p.temp + scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural'))
-p.temp <- p.temp + annotate('text',x = 1, y = 0.58, label = 'a')
-p.temp <- p.temp + annotate('text', x = c(2, 3), y = c(0.58, 0.58), label = 'b')
+p.temp  <-
+  bird.long %>% 
+  ggplot(aes(x = majority, y = Temp_Var, colour = majority)) +
+  theme_classic(base_size = 14) + 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 14, colour = 'black'),
+        axis.title.y = element_text(size = 14)) +
+  labs(x = NULL, y = 'Compositional Variability') + 
+  geom_point(aes(shape = majority),col = 'black',
+             position = position_jitter(width = 0.35),
+             size = 2,
+             show.legend = FALSE) + 
+  geom_boxplot(aes(fill = majority), col = 'black',
+               alpha = 0.6) + 
+  scale_color_manual(values = c('#fd6f77','#bd95b2','#00b0f0'),aesthetics = c('colour','fill')) +
+  #scale_color_brewer(palette = "Dark2", direction = -1,aesthetics = c('colour','fill')) + 
+  scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural')) + 
+  annotate('text',x = 1, y = 0.58, label = 'a') + 
+  annotate('text', x = c(2, 3), y = c(0.58, 0.58), label = 'b') +
+  scale_y_continuous(breaks = breaks_pretty(n = 5)) + 
+  scale_shape_manual(values = c(22,2,1)) +
+  theme(legend.position = "none")
 
 ## Functional variability
 gls.cov <- gls(log(CoV) ~ majority, data = bird.long,
                corr = corGaus(form = ~xcoord + ycoord, nugget = TRUE))
 ci.cov <- confint(glht(gls.cov, linfct = mcp(majority = "Tukey"))) # agri and urabna re significantly diff from eachother
 
-p.cov <- ggplot(bird.long, aes(x = majority, y = log(CoV), colour = majority)) + geom_boxplot()
-p.cov <- p.cov + xlab(NULL) + ylab('Functional Temporal Variability (log)')
-p.cov <- p.cov + scale_color_brewer(palette = "Dark2", direction = -1)
-p.cov <- p.cov + theme_classic() + theme(legend.position = 'none', 
-                                         axis.text = element_text(size = 14),
-                                         axis.title.y = element_text(size = 14))
-p.cov <- p.cov + scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural'))
-p.cov <- p.cov + annotate('text',x = 1, y = 0.7, label = 'a')
-p.cov <- p.cov + annotate('text', x = 2, y = 0.7, label = 'b')
-p.cov <- p.cov + annotate('text', x = 3, y = 0.7, label = 'ab')
+p.cov  <-
+  bird.long %>% 
+  ggplot(aes(x = majority, y = log(CoV), colour = majority)) +
+  theme_classic(base_size = 14) + 
+  theme(legend.position = 'none',
+        axis.text = element_text(size = 14, colour = 'black'),
+        axis.title.y = element_text(size = 14)) +
+  labs(x = NULL, y = 'Functional Variability (ln)') + 
+  geom_point(aes(shape = majority),col = 'black',
+             position = position_jitter(width = 0.35),
+             size = 2,
+             show.legend = FALSE) + 
+  geom_boxplot(aes(fill = majority), col = 'black',
+               alpha = 0.6) + 
+  scale_color_manual(values = c('#fd6f77','#bd95b2','#00b0f0'),aesthetics = c('colour','fill')) +
+  #scale_color_brewer(palette = "Dark2", direction = -1,aesthetics = c('colour','fill')) + 
+  scale_x_discrete(labels = c('Artificial', 'Agriculture', 'Forest and \n semi-natural')) + 
+  annotate('text',x = 1, y = 0.7, label = 'a') + 
+  annotate('text', x = 2, y = 0.7, label = 'b') +
+  annotate('text', x = 3, y = 0.7, label = 'ab') +
+  scale_y_continuous(breaks = breaks_pretty(n = 5)) + 
+  scale_shape_manual(values = c(22,2,1)) +
+  theme(legend.position = "none")
 
-patch <- p.response + p.effect + p.effectvar + p.asyn + p.temp + p.cov
-patch <- patch + plot_annotation(tag_levels = "a", tag_suffix = ')') + plot_layout(ncol = 2)
+library(patchwork)
+patch <- (p.response + p.asyn) / (p.temp + p.cov) / (p.effect + p.effectvar) +  plot_annotation(tag_levels = "a", tag_suffix = ')')
 
-tiff('D:\\ResponseEffectDiversity\\ResultsAndFigures\\LandcoverFigures\\LandcoverBoxplots.tiff', res = 360,
-     height = 10, width = 10, unit = 'in', compression = 'lzw')
+#tiff('D:\\ResponseEffectDiversity\\ResultsAndFigures\\LandcoverFigures\\LandcoverBoxplots.tiff', res = 360,
+#     height = 10, width = 10, unit = 'in', compression = 'lzw')
 #cowplot::plot_grid(p.response, p.effect, p.effectvar, p.asyn, p.temp, p.cov, nrow = 3,
 #                   labels = c('a)', 'b)', 'c)', 'd)', 'e)', 'f)'))
 patch
@@ -154,8 +236,15 @@ cov2 <- gls(CoV ~ response.fdis*majority, data = bird.long,
 
 library(sjPlot)
 colpal <- RColorBrewer::brewer.pal(3, "Dark2")[3:1]
-p.int <- plot_model(cov2, type = 'int', show.values = TRUE, title = '', colors = colpal) + theme_classic() + theme(legend.title = element_blank()) 
-p.int <- p.int + xlab('Response trait FDis') + ylab('Functional Variability') 
+p.int <- plot_model(cov2, 
+                    type = 'int', 
+                    show.values = TRUE, 
+                    title = '', 
+                    #colors = colpal,
+                    colors = c('#fd6f77','#bd95b2','#00b0f0')) + 
+  theme_classic() + 
+  theme(legend.title = element_blank(),legend.position = 'top') + 
+  xlab('Response diversity') + ylab('Functional Variability') 
 
 tiff('D:\\ResponseEffectDiversity\\ResultsAndFigures\\LandcoverFigures\\CoVInteraction.tiff', res = 360,
      height = 7, width = 7, unit = 'in', compression = 'lzw')
